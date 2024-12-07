6281d463e62f437af1bcc88d9d870ecfa0aca79f
thaddeus kuah <tk@tkkr.dev>
2024-12-07 19:21:40 +0000

    feat: move login scripts to separate file
    Signed-off-by: GitHub <noreply@github.com>

---
+++ static/js/login.js
@@ -0,0 +1,67 @@
+$(function () {
+    const phoneInput = $('#phone');
+    const otpInput = $('#otp');
+    phoneInput.on('input', () => {
+        phoneInput.val(phoneInput.val().replace(/[^0-9]/g, ''));
+        if (phoneInput.val().length > 8) phoneInput.val(phoneInput.val().slice(0, 8));
+    });
+    otpInput.on('input', () => {
+        otpInput.val(otpInput.val().replace(/[^0-9]/g, ''));
+        if (otpInput.val().length > 6) otpInput.val(otpInput.val().slice(0, 6));
+    });
+})
+
+function login() {
+    const phoneInput = $('#phone');
+    const phone = phoneInput.val();
+    if (phone.length < 8) {
+        $('#message-box').removeClass('hidden').removeClass('bg-green').addClass('bg-red');
+        $('#message-content').text('Please enter a valid phone number.');
+    } else {
+        $('#message-box').removeClass('hidden').removeClass('bg-red').addClass('bg-green');
+        $('#message-content').html('An OTP was sent to you via WhatsApp.<br>Please enter the code above to sign in.');
+        $('#otp-div').removeClass('hidden');
+        $('#login-button').text('Verify OTP');
+        $('#phone').prop('disabled', true);
+        $('#login-button').attr('onclick', 'verifyOTP()');
+        fetch('./otp', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                phone: phone
+            })
+        })
+    }
+}
+function verifyOTP() {
+    const phoneInput = $('#phone');
+    const otpInput = $('#otp');
+    const phone = phoneInput.val();
+    const otp = otpInput.val();
+    if (otp.length < 6) {
+        $('#message-box').removeClass('hidden').removeClass('bg-green').addClass('bg-red');
+        $('#message-content').text('Please enter a valid OTP.');
+    } else {
+        fetch('./verify_otp', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                phone: phone,
+                otp: otp
+            })
+        }).then(async response => {
+            const json = await response.json();
+            if (json.status === 200) {
+                $('#message-box').removeClass('hidden').removeClass('bg-red').addClass('bg-green');
+                $('#message-content').text('Logged in, redirecting...');
+            } else {
+                $('#message-box').removeClass('hidden').removeClass('bg-green').addClass('bg-red');
+                $('#message-content').text('Invalid OTP. Please try again.');
+            }
+        });
+    }
+}
