98674312a04e358f9478b9294ae79b2393fe04b8
thaddeus kuah <tk@tkkr.dev>
2024-12-07 19:19:30 +0000

    feat: add current routes
    Signed-off-by: GitHub <noreply@github.com>

---
+++ main.py
@@ -0,0 +1,71 @@
+import os
+import requests
+import secrets
+import shelve
+from dotenv import load_dotenv
+from flask import Flask, request, render_template, jsonify
+
+load_dotenv()
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = os.getenv("DEBUG") == "True"
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/otp", methods=["POST"])
+def otp():
+    data = request.get_json()
+    phone = data.get("phone")
+    otp = secrets.randbelow(10**6)
+    with shelve.open("logins") as logins:
+        while otp in logins.values():
+            otp = secrets.randbelow(10**6)
+        logins[phone] = str(otp)
+    requests.post(
+        "https://develop.tkkr.dev/otp",
+        json={"to": f"65{phone}", "from": "pay2live", "otp": otp},
+        headers={"Authorization": os.getenv("OTP_TOKEN")},
+    )
+    return jsonify(
+        {
+            "status": 200,
+            "phone": phone,
+            "message": f"OTP sent to {phone}",
+        }
+    )
+
+
+@app.route("/verify_otp", methods=["POST"])
+def verify_otp():
+    data = request.get_json()
+    otp = data.get("otp")
+    phone = data.get("phone")
+    with shelve.open("logins") as logins:
+        if phone in logins.keys() and logins[phone] == otp:
+            return jsonify(
+                {
+                    "status": 200,
+                    "phone": phone,
+                    "message": "OTP verified",
+                }
+            )
+        else:
+            return jsonify(
+                {
+                    "status": 400,
+                    "message": "Invalid OTP",
+                }
+            )
+
+
+if __name__ == "__main__":
+    app.run(debug=(os.getenv("DEBUG") == "True"))
