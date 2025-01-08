736bebcf5136795c41e8b4c234d06f090b5be133
thaddeus kuah <tk@tkkr.dev>
2025-01-08 09:42:24 +0000

    feat: import extra routes
    Signed-off-by: GitHub <noreply@github.com>

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,9 @@
 from .index import *
 from .about import *
 from .shop import *
+from .appointments import *
+from .calendar import *
 from .login import *
 from .api import *
 
-__all__ = ["index", "about", "shop", "login", "api"]
+__all__ = ["index", "about", "shop", "appointments", "calendar", "login", "api"]
