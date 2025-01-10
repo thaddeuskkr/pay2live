346cdfae3b5eecfe97ab82484a040c55cd686589
thaddeus kuah <tk@tkkr.dev>
2025-01-10 18:53:26 +0000

    feat: add registration route
    Signed-off-by: GitHub <noreply@github.com>

--- routes/api/register.py
+++ routes/api/register.py
@@ -1,5 +1,5 @@
 from flask import request, make_response
-from app import app, logins
+from app import app, users
 
 
 @app.route("/api/register", methods=["POST"])
@@ -9,10 +9,8 @@ def register():
         "first_name",
         "last_name",
         "email",
-        "phone",
         "gender",
         "nric",
-        "admin",
         "role",
         "address",
     ] not in data:
@@ -20,20 +18,69 @@ def register():
     first_name: str = data.get("first_name")
     last_name: str = data.get("last_name")
     email: str = data.get("email")
-    phone: str = data.get("phone")
     gender: str = data.get("gender")
     nric: str = data.get("nric")
     role: str = data.get("role")
     address: str = data.get("address")
-    login = logins.find_one({"phone": phone})
     if (
         len(first_name) < 1
         or len(last_name) < 1
         or len(email) < 1
-        or len(phone) < 1
         or len(gender) <= 0
         or len(nric) < 1
         or len(role) < 1
         or len(address) < 1
     ):
         return make_response({"error": "Invalid text in input fields"}, 400)
+    if "session_token" in request.cookies:
+        if len(request.cookies["session_token"]) > 5:
+            auth = request.cookies["session_token"]
+        else:
+            response = make_response(
+                {
+                    "message": "Invalid session token",
+                },
+                401,
+            )
+            return response
+    else:
+        response = make_response(
+            {
+                "message": "No session token found",
+            },
+            401,
+        )
+        return response
+    user = users.find_one({"session_token": auth})
+    if user is None:
+        response = make_response(
+            {
+                "message": "Invalid session token",
+            },
+            401,
+        )
+        return response
+    if user and user["registered"] == True:
+        return make_response(
+            {"error": "User with that phone number is already registered"}, 400
+        )
+    if users.find_one({"email": email}):
+        return make_response(
+            {"error": "User with that email address is already registered"}, 400
+        )
+    users.update_one(
+        {"session_token": auth},
+        {
+            "$set": {
+                "first_name": first_name,
+                "last_name": last_name,
+                "email": email,
+                "gender": gender,
+                "nric": nric,
+                "role": role,
+                "address": address,
+                "registered": True,
+            }
+        },
+    )
+    return make_response({"message": "User registered successfully"}, 200)
